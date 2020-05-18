41179405982a6ae0b32b6a07937f645cdbca78e1
Ion Creciun <ion.creciun1@gmail.com>
2020-05-18 11:36:41 +0200

    create book

---
+++ Client/src/model/Book.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.io.Serializable;
+
+public class Book implements Serializable
+{
+  private String title;
+  private String author;
+  private String language;
+  private String book_type;
+  private String description;
+
+}
