8af4fec84ae026ab0eb1140cf67d0d81a67f9902
Ion Creciun <ion.creciun1@gmail.com>
2020-05-22 11:57:45 +0200

    Java doc

--- Client/src/model/AbstractBookFactory.java
+++ Client/src/model/AbstractBookFactory.java
@@ -3,20 +3,30 @@ package model;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Factory for book that store title and author
+ */
 public class AbstractBookFactory
 {
   private static Map<String,AbstractBook> bookMap = new HashMap<>();
+
+  /**
+   * Getting an Abstract book by  title and synchronize it so it will be thread safe
+   * @param title title of abstract book
+   * @param author author of abstract book
+   * @return an AbstractBook
+   */
   public static AbstractBook getBookFly(String title,String author)
   {
     AbstractBook book = bookMap.get(title);
-    if(book == null)
+    if(book == null) /*lazy instantiation*/
     {
-      synchronized (bookMap)
+      synchronized (bookMap)/*thread safe*/
       {
         book = bookMap.get(title);
         if (book == null)
         {
-          book = new BookFly(title, author);
+          book = new BookFly(title, author);/*creates an instance of a book and puts it into the hashmap*/
           bookMap.put(title, book);
         }
       }
--- Server/src/model/AbstractBookFactory.java
+++ Server/src/model/AbstractBookFactory.java
@@ -3,9 +3,19 @@ package model;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Factory for book that store title and author
+ */
 public class AbstractBookFactory
 {
   private static Map<String,AbstractBook> bookMap = new HashMap<>();
+
+  /**
+   * Getting an Abstract book by  title and synchronize it so it will be thread safe
+   * @param title title of abstract book
+   * @param author author of abstract book
+   * @return an AbstractBook
+   */
   public static AbstractBook getBookFly(String title,String author)
   {
     AbstractBook book = bookMap.get(title);
