f610a7da2478831173903359025feb6d7adccd96
Albert-Andrei <moldovanualiberta@gmial.com>
2020-05-22 10:45:27 +0200

    just in case

--- Client/src/model/BookFly.java
+++ Client/src/model/BookFly.java
@@ -2,9 +2,15 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * * A class representing a date. *
+ * * @author Steffen Vissing Andersen *
+ * @version 3.2 – November 2019
+ *
+ * */
+
 public class BookFly extends AbstractBook
 {
-
   public BookFly(String title, String author)
   {
     super(title, author);
