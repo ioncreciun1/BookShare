c0c41b8a0c4466203bf7608daed421c1f2c2ba53
Samantha-Nicehouse <38719500+Samantha-Nicehouse@users.noreply.github.com>
2020-05-29 10:23:28 +0200

    529work

--- Server/src/Database/UserDAOImplementation.java
+++ Server/src/Database/UserDAOImplementation.java
@@ -21,8 +21,7 @@ public class UserDAOImplementation implements UserDAO
   private Connection getConnection() throws SQLException
   {
     return DriverManager
-        .getConnection("jdbc:postgresql://btv9bsc3ws7gpfnylr1c-postgresql.services.clever-cloud.com:5432/btv9bsc3ws7gpfnylr1c", "uhxksonl4uvuqdftznsg",
-            "pnwEWl0YlSc6A2z619ff");
+        .getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "2011");
 
   }
 /**checks if the email entered in the sign up form is already in the database, return false and adds the user if it is not already in use
